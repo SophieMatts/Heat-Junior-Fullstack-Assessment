bbdfa3144e90fb1030aadd9f258fc8c103506f21
sophie mattacks <s.mattacks@hotmail.co.uk>
2023-05-29 17:21:01 +0100

    [Q6] Featured pets have a gold border

--- fullstack-frontend/assets/scripts/script.js
+++ fullstack-frontend/assets/scripts/script.js
@@ -18,7 +18,7 @@ class Pet {
 
   generateCard() {
     return `
-      <div class="pets__card">
+      <div class="${this.featured ? 'pets__featured_card': 'pets__card'}">
         <h2 class="pets__card__title">${this.name}</h2>
         <p class="pets__card__info">Species: ${this.species}</p>
         <p class="pets__card__info">Age: ${this.age}</p>
