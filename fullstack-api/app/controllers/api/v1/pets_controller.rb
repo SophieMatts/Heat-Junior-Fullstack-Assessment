4a0d18fa6aa87885afd027eed550a1faba22532d
sophie mattacks <s.mattacks@hotmail.co.uk>
2023-05-29 17:20:39 +0100

    [Q3] Pet records displayed alphabetically and featured first

--- fullstack-api/app/controllers/api/v1/pets_controller.rb
+++ fullstack-api/app/controllers/api/v1/pets_controller.rb
@@ -6,7 +6,7 @@ module Api
     class PetsController < ApplicationController
 
       def index
-        pets = Pet.all
+        pets = Pet.all.order(:featured, :name)
         render json: pets, status: :ok
       end
 
